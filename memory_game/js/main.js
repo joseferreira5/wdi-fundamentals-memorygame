e8c7e6decaa402fa31b6c113d2222479f188e245
Jose Ferreira <jferreira590@gmail.com>
2020-02-03 11:39:27 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+const cardOne = 'queen';
+const cardTwo = 'queen';
+const cardThree = 'king';
+const cardFour = 'king';
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
