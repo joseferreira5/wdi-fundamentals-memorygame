c7f7dab07acf0368d9014b27ffaeab9cde8e54b1
Jose Ferreira <jferreira590@gmail.com>
2020-02-03 16:54:49 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-const cardOne = 'queen';
-const cardTwo = 'queen';
-const cardThree = 'king';
-const cardFour = 'king';
+const cards = ["queen", "queen", "king", "king");
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardtwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay [1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
